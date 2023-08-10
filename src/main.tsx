930180de35c878160176ec570d6757a36a4f7c37
Gustavo <gustavoluizmiranda405@gmail.com>
2023-08-09 21:55:52 -0300

    organização dos componentes header e footer

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom/client'
 //componentes
 import Home from './pages/Home/'
 import ListaServicos from './pages/ListaServicos'
+import Header from './componentes/Header'
+import Footer from './componentes/Footer'
 
 //estização global
 import "./Index.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <Home /> */}
-    <ListaServicos />
+    <Header/>
+    <Home />
+    <Footer/>
   </React.StrictMode>,
 )
