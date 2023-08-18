736d5abb21d062ef0c79c83637735d77299591ae
Gustavo <gustavoluizmiranda405@gmail.com>
2023-08-17 21:57:15 -0300

    API adicionada na página lista de devs e lista de serviços

--- vsconnect/src/pages/ListaServicos/index.tsx
+++ vsconnect/src/pages/ListaServicos/index.tsx
@@ -1,68 +1,57 @@
 import "./style.css";
 
 //components
-import CardDev from "../../components/CardServico";
-
-//hooks
-import { useState } from "react";
 import CardServico from "../../components/CardServico";
 
-function ListaServicos() {
-    const [servicos, setServicos] = useState<any[]>([
-        {
-            titulo: "Desenvolvimento de site institucional - Gateway de Pagamento / Fintech",
-            descricao: "Desenvolver um site responsivo que seja utilizado como uma plataforma de apresentação do nosso gateway de pagamento. O objetivo principal deste projeto é criar um site atraente e informativo, que demonstre as funcionalidades e benefícios do nosso gateway de pagamento para potenciais clientes.",
-            proposta: 1300,
-            techs: [
-                "HTML",
-                "CSS",
-                "REACT"
-            ]
-        },
-        {
-            titulo: "Bot telegram Pagamento",
-            descricao: "Preciso fazer um código em python para um bot do telegram. O bot será para solicitação de pagamento.",
-            proposta: 2400,
-            techs: [
-                "PYTHON"
-            ]
-        },
-        {
-            titulo: "Caixa Rápido",
-            descricao: "Preciso fazer um software que permita ao usuário fazer o upload de seu extrato bancário em formato( ofx). Dentro do software o mesmo poderá categorizar todas as suas receitas e despesas, tendo categorias sugeridas pelo software e permitindo também personalizações. Após o lançamento de vários extratos o software irá entender que são lançamentos parecidos e fará a categorização de maneira automática, cabendo ao usuário somente categorizar as receitas e despesas que não se repetem. Após a categorização o software irá emitir gráficos e relatórios baseados na categorização das contas.",
-            proposta: 1200,
-            techs: [
-                "PYTHON"
-            ]
-        }
-    ]);
+//hooks
+import { useEffect, useState } from "react";
 
-    const [listaServicosFiltrados, setListaServicosFiltrados] = useState<any[]>(servicos);
+import api from "../../utils/api";
+
+function ListaServicos() {
+    const [servicos, setServicos] = useState<any[]>([]);
 
     const [tecnologiaDigitada, setTecnologiaDigitada] = useState<string>("");
 
     //função onde pega o que o usuario digitou
     function verificarCampoTechs(event: any) {
         if (event.target.value === "") {
-            setListaServicosFiltrados(servicos);
+            listarServicos();
         }
         setTecnologiaDigitada(event.target.value);
     }
-    function buscarDevPorTechs(event: any) {
+    function buscarServicoPorTechs(event: any) {
         //não recarrega a pagina
         event.preventDefault();
 
         //filtrar devs pela skill digitada no campo buscar
-        const servicosFiltrados = servicos.filter((servico: any) => servico.skills.includes(tecnologiaDigitada.toLocaleUpperCase()));
+        const servicosFiltrados = servicos.filter((servico: any) => servico.techs.includes(tecnologiaDigitada.toLocaleUpperCase()));
 
         if (servicosFiltrados.length === 0) {
             alert("Nenhum desenvolvedor(a) com essa skill :(")
         } else {
             //atribui valor de devs filtrado, ao state ListaDevsFiltrados 
-            setListaServicosFiltrados(servicosFiltrados);
+            setServicos(servicosFiltrados);
         }
 
     }
+
+    function listarServicos(){
+        api.get("servicos").then((response: any) => {
+            console.log(response);
+
+            setServicos(response.data)
+        })
+        .catch((error: any)=> {
+            console.log("Error ao realizar uma requisição: ", error);
+        })
+    }
+
+    useEffect(() => {
+        //executa ação após o componente ser recarregado
+        listarServicos();
+    }, [])
+
     return (
         <>
             <main id="main_listaservicos">
@@ -70,7 +59,7 @@ function ListaServicos() {
                     <div className="lista_servicos_conteudo">
                         <h1>Lista de Serviços</h1>
                         <hr />
-                        <form method="post" onSubmit={buscarDevPorTechs}>
+                        <form method="post" onSubmit={buscarServicoPorTechs}>
                             <div className="wrapper_form">
                                 <label htmlFor="busca">Procurar serviços</label>
                                 <div className="campo-label">
@@ -88,12 +77,12 @@ function ListaServicos() {
                         <div className="wrapper_lista">
                             <ul>
                                 {
-                                    listaServicosFiltrados.map((servicos: any, indice: number) => {
+                                    servicos.map((servicos: any, indice: number) => {
                                         return <li key={indice}>
                                             <CardServico
-                                                titulo={servicos.titulo}
-                                                valor={servicos.descricao}
-                                                descricao={servicos.proposta}
+                                                titulo={servicos.nome}
+                                                valor={servicos.valor}
+                                                descricao={servicos.descricao}
                                                 listaTechs={servicos.techs}
                                             />
                                         </li>
